6e832b19e6376d54067542d2975c8ebc490350d7
Yohannes Teshome <katteyohannes@gmail.com>
2023-08-20 01:42:53 -0700

    test ffmpeg

--- script.py
+++ script.py
@@ -1,2 +1,22 @@
+from ffmpeg import FFmpeg
 
-print("av converter")
+
+def main():
+    ffmpeg = (
+        FFmpeg()
+        .option("y")
+        .input("assets/video/sample.mp4")
+        .output(
+            "ouptut.mp4",
+            {"codec:v": "libx264"},
+            vf="scale=1280:-1",
+            preset="veryslow",
+            crf=24,
+        )
+    )
+
+    ffmpeg.execute()
+
+
+if __name__ == "__main__":
+    main()
