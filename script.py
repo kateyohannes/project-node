03dded82f67e87e09c6ad4f330036ca0f7ba12f4
Yohannes Teshome <katteyohannes@gmail.com>
2023-08-28 23:09:57 -0700

    preset to slow

--- script.py
+++ script.py
@@ -13,7 +13,7 @@ async def main():
             "assets/video/out/ouptut.mp4",
             {"codec:v": "libx264"},
             vf="scale=1280:-1",
-            preset="fast",
+            preset="slow",
             crf=24,
         )
     )
